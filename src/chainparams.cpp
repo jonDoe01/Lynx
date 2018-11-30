3881a480f4940a4d7ca99596fdae205ca3b465b7
ilya_avdonin <ilya_avdonin@relex.ru>
2018-11-30 21:07:41 +0300

    Use legacy (K*) addresses by default. Restored timeout for applying Consensus::DEPLOYMENT_SEGWIT feature broken during latest merge with upstream

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -126,8 +126,8 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1485561600; // January 28, 2017
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1517356801; // January 31st, 2018
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 0;
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 999999999999ULL;
 
         // The best chain should have at least this much work.
         consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000002025852fc35a72h");
